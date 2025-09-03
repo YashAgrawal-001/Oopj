b321e252a8f54ec438e06b9445219788ca0b3897
yash agrawal <yashdevagrawal2006@gmail.com>
2025-09-03 09:05:23 +0530

    Create prectical6.java

---
+++ HelloWorld/prectical6.java
@@ -0,0 +1,13 @@
+class ThisKeyword {
+    int x;
+    ThisKeyword(int x) {
+        this.x = x; // 'this' refers to current object
+    }
+    void display() {
+        System.out.println("Value of x: " + this.x);
+    }
+    public static void main(String[] args) {
+        ThisKeyword obj = new ThisKeyword(10);
+        obj.display();
+    }
+}
